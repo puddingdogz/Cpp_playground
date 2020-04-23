b4738132747c14e25bec813c3eb49c648e419e07
chunjen <puddingdogz@gmail.com>
2020-04-22 18:06:30 -0700

    Example00-HelloWorld

---
+++ ex00_HelloWorld/ex00_HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ex00_HelloWorld
+//
+//  Created by Chun-Jen Hsueh on 4/22/20.
+//  Copyright © 2020 Chun-Jen Hsueh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
